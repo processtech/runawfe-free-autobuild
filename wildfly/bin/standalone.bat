305eec617c55a73f55a905837902dee3ff3a0b4e
aleksey <aleksey.konst@gmail.com>
2018-10-29 00:23:57 +0300

    If JAVA_HOME is not set use java from path

--- wildfly/bin/standalone.bat
+++ wildfly/bin/standalone.bat
@@ -122,8 +122,10 @@ if "x%JAVA_HOME%" == "x" (
   echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
 ) else (
   if not exist "%JAVA_HOME%" (
-    echo JAVA_HOME "%JAVA_HOME%" path doesn't exist
-    goto END
+    echo JAVA_HOME "%JAVA_HOME%" path doesn't exist. Ignoring JAVA_HOME.
+    echo JAVA_HOME is not set. Unexpected results may occur.
+    echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+    set  JAVA=java
   ) else (
     echo Setting JAVA property to "%JAVA_HOME%\bin\java"
     set "JAVA=%JAVA_HOME%\bin\java"
