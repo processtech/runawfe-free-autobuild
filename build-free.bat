9be7e5fb79f52b0026000126af07d6a4848fa883
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-04-05 16:30:32 +0700

    Interrupt build when error occured

--- build-free.bat
+++ build-free.bat
@@ -17,99 +17,99 @@ mkdir build
 mkdir %RESULTS_DIR%
 
 rem Copy required zip files and folders (jboss and so on) into build directory
-jar -cMf wildfly.zip wildfly
-move wildfly.zip build
+jar -cMf wildfly.zip wildfly || exit /b 1
+move wildfly.zip build || exit /b 1
 
 copy readme build
 
 
 rem Export source code
-cd /D build
+cd /D build || exit /b 1
 git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-server.git source/projects/wfe
-cd source/projects/wfe
-git checkout %GIT_BRANCH_NAME%
+cd source/projects/wfe || exit /b 1
+git checkout %GIT_BRANCH_NAME% || exit /b 1
 
 git rev-parse HEAD > tmp-hash.txt
 set /p BUILD_HASH=<tmp-hash.txt
 del tmp-hash.txt
 
-cd ../../../
-rd /S /Q source\projects\wfe\.git
-git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-devstudio.git source/projects/gpd
-cd source/projects/gpd
-git checkout %GIT_BRANCH_NAME%
-cd ../../../
+cd ../../../ || exit /b 1
+rd /S /Q source\projects\wfe\.git || exit /b 1
+git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-devstudio.git source/projects/gpd || exit /b 1
+cd source/projects/gpd || exit /b 1
+git checkout %GIT_BRANCH_NAME% || exit /b 1
+cd ../../../ || exit /b 1
 rd /S /Q source\projects\gpd\.git
-git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-notifier-java.git source/projects/rtn
-cd source/projects/rtn
-git checkout %GIT_BRANCH_NAME%
-cd ../../../
+git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-notifier-java.git source/projects/rtn || exit /b 1
+cd source/projects/rtn || exit /b 1
+git checkout %GIT_BRANCH_NAME% || exit /b 1
+cd ../../../ || exit /b 1
 rd /S /Q source\projects\rtn\.git
-git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-installer.git source/projects/installer
-cd source/projects/installer
-git checkout %GIT_BRANCH_NAME%
-cd ../../../
+git clone %GIT_SOURCE_URL%/runawfe-%GIT_PROJECT_EDITION%-installer.git source/projects/installer || exit /b 1
+cd source/projects/installer || exit /b 1
+git checkout %GIT_BRANCH_NAME% || exit /b 1
+cd ../../../ || exit /b 1
 rd /S /Q source\projects\installer\.git
 
-mkdir source\docs
-mkdir source\docs\guides
+mkdir source\docs || exit /b 1
+mkdir source\docs\guides || exit /b 1
 copy readme source\docs\guides\
 
 rem Update projects version
-cd source\projects\installer\windows\
-call mvn versions:set -DnewVersion=%WFE_VERSION%
-cd ../../wfe/wfe-appserver
-call mvn versions:set -DnewVersion=%WFE_VERSION%
-cd ../wfe-webservice-client
-call mvn versions:set -DnewVersion=%WFE_VERSION%
-cd ../wfe-app
-call mvn versions:set -DnewVersion=%WFE_VERSION%
+cd source\projects\installer\windows\ || exit /b 1
+call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
+cd ../../wfe/wfe-appserver || exit /b 1
+call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
+cd ../wfe-webservice-client || exit /b 1
+call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
+cd ../wfe-app || exit /b 1
+call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
 rem cd ../wfe-remotebots
 rem call mvn versions:set -DnewVersion=%WFE_VERSION%
-cd ../../rtn
-call mvn versions:set -DnewVersion=%WFE_VERSION%
-cd ../gpd/plugins
-call mvn tycho-versions:set-version -DnewVersion=%WFE_VERSION%
+cd ../../rtn || exit /b 1
+call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
+cd ../gpd/plugins || exit /b 1
+call mvn tycho-versions:set-version -DnewVersion=%WFE_VERSION% || exit /b 1
 
-cd ..\..\..\..\
-jar -cMf source.zip source
-mkdir %RESULTS_DIR%\source
-move source.zip %RESULTS_DIR%\source\source-%WFE_VERSION%.zip
+cd ..\..\..\..\ || exit /b 1
+jar -cMf source.zip source || exit /b 1
+mkdir %RESULTS_DIR%\source || exit /b 1
+move source.zip %RESULTS_DIR%\source\source-%WFE_VERSION%.zip || exit /b 1
 
-cd source\projects\installer\windows\
+cd source\projects\installer\windows\ || exit /b 1
 rem Build distr
-call mvn clean package -Dwfe.edition=%WFE_EDITION% -Dwfe.buildhash=%BUILD_HASH% -Djdk.dir="%~dp0jdk" -Dstatistic.report.url=%STATISTIC_REPORT_URL% -Dstatistic.report.days.after.error=%STATISTIC_REPORT_DAYS_AFTER_ERROR%
+call mvn clean package -Dwfe.edition=%WFE_EDITION% -Dwfe.buildhash=%BUILD_HASH% -Djdk.dir="%~dp0jdk" -Dstatistic.report.url=%STATISTIC_REPORT_URL% -Dstatistic.report.days.after.error=%STATISTIC_REPORT_DAYS_AFTER_ERROR% || exit /b 1
 
-xcopy /E /Q target\test-result %RESULTS_DIR%\test-result\
-mkdir %RESULTS_DIR%\Execution\wildfly
-copy target\artifacts\Installer64\wildfly\RunaWFE-Installer.exe %RESULTS_DIR%\Execution\wildfly\RunaWFE-%WFE_VERSION%.exe
+xcopy /E /Q target\test-result %RESULTS_DIR%\test-result\ || exit /b 1
+mkdir %RESULTS_DIR%\Execution\wildfly || exit /b 1
+copy target\artifacts\Installer64\wildfly\RunaWFE-Installer.exe %RESULTS_DIR%\Execution\wildfly\RunaWFE-%WFE_VERSION%.exe || exit /b 1
 
-mkdir %RESULTS_DIR%\bin
-mkdir %RESULTS_DIR%\bin\server
+mkdir %RESULTS_DIR%\bin || exit /b 1
+mkdir %RESULTS_DIR%\bin\server || exit /b 1
 rem Create bin file for wildfly server
-jar xf target\artifacts\wildfly\app-server\wfe-appserver-base-%WFE_VERSION%.zip 
-jar xf target\artifacts\wildfly\app-server\wfe-appserver-diff-%WFE_VERSION%.zip 
-xcopy /E /Q ..\simulation\* jboss\
-move jboss wildfly
-jar -cMf runawfe-wildfly-%WFE_VERSION%.zip wildfly
-rd /S /Q wildfly
-move runawfe-wildfly-%WFE_VERSION%.zip %RESULTS_DIR%\bin\server\runawfe-wildfly-%WFE_VERSION%.zip
+jar xf target\artifacts\wildfly\app-server\wfe-appserver-base-%WFE_VERSION%.zip || exit /b 1
+jar xf target\artifacts\wildfly\app-server\wfe-appserver-diff-%WFE_VERSION%.zip || exit /b 1
+xcopy /E /Q ..\simulation\* jboss\ || exit /b 1
+move jboss wildfly || exit /b 1
+jar -cMf runawfe-wildfly-%WFE_VERSION%.zip wildfly || exit /b 1
+rd /S /Q wildfly || exit /b 1
+move runawfe-wildfly-%WFE_VERSION%.zip %RESULTS_DIR%\bin\server\runawfe-wildfly-%WFE_VERSION%.zip || exit /b 1
 
 rem Create bin file for gpd
-xcopy /E /Q target\artifacts\gpd\all %RESULTS_DIR%\bin\gpd\
+xcopy /E /Q target\artifacts\gpd\all %RESULTS_DIR%\bin\gpd\ || exit /b 1
 
 rem Create bin file for rtn 
-mkdir %RESULTS_DIR%\bin\rtn
+mkdir %RESULTS_DIR%\bin\rtn || exit /b 1
 
-xcopy /E /Q target\artifacts\rtn\64 rtn\
-jar -cMf runawfe-rtn-win64-%WFE_VERSION%.zip rtn\
+xcopy /E /Q target\artifacts\rtn\64 rtn\ || exit /b 1
+jar -cMf runawfe-rtn-win64-%WFE_VERSION%.zip rtn\ || exit /b 1
 rd /S /Q rtn
-move runawfe-rtn-win64-%WFE_VERSION%.zip %RESULTS_DIR%\bin\rtn\runawfe-rtn-win64-%WFE_VERSION%.zip
+move runawfe-rtn-win64-%WFE_VERSION%.zip %RESULTS_DIR%\bin\rtn\runawfe-rtn-win64-%WFE_VERSION%.zip || exit /b 1
 
 
-xcopy /E /Q target\artifacts\rtn\linux64 rtn\
-jar -cMf runawfe-rtn-linux64-%WFE_VERSION%.zip rtn\
-rd /S /Q rtn
-move runawfe-rtn-linux64-%WFE_VERSION%.zip %RESULTS_DIR%\bin\rtn\runawfe-rtn-linux64-%WFE_VERSION%.zip
+xcopy /E /Q target\artifacts\rtn\linux64 rtn\ || exit /b 1
+jar -cMf runawfe-rtn-linux64-%WFE_VERSION%.zip rtn\ || exit /b 1
+rd /S /Q rtn || exit /b 1
+move runawfe-rtn-linux64-%WFE_VERSION%.zip %RESULTS_DIR%\bin\rtn\runawfe-rtn-linux64-%WFE_VERSION%.zip || exit /b 1
 
 echo Autobuild finished.
