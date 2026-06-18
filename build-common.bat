ad7d42e3439c07caadc909bc63a25a2982be86f5
Andrey Pletenev <pletenev.andrey@yandex.ru>
2026-06-18 11:29:24 +0700

    Improve benchmarking

--- build-common.bat
+++ build-common.bat
@@ -1,5 +1,7 @@
 @echo Starting autobuild at %TIME%
 @for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "START_TICKS=%%i"
+@for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "STAGE1_TICKS=%%i"
+@powershell -NoProfile -Command "Write-Host '=== Stage 1/6: Prepare ===' -ForegroundColor Blue"
 
 @echo Clean artifacts from previous builds
 rd /S /Q build 
@@ -16,6 +18,8 @@ move wildfly.zip build || exit /b 1
 copy readme build
 
 
+@for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "STAGE2_TICKS=%%i"
+@powershell -NoProfile -Command "Write-Host '=== Stage 2/6: Clone ===' -ForegroundColor Blue"
 echo Export source code
 cd /D build || exit /b 1
 
@@ -58,6 +62,8 @@ mkdir source\docs || exit /b 1
 mkdir source\docs\guides || exit /b 1
 copy readme source\docs\guides\
 
+@for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "STAGE3_TICKS=%%i"
+@powershell -NoProfile -Command "Write-Host '=== Stage 3/6: Update version ===' -ForegroundColor Blue"
 echo Update projects version
 cd source\projects\installer\windows\ || exit /b 1
 call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
@@ -74,16 +80,21 @@ call mvn versions:set -DnewVersion=%WFE_VERSION% || exit /b 1
 cd ../gpd/plugins || exit /b 1
 call mvn tycho-versions:set-version -DnewVersion=%WFE_VERSION% || exit /b 1
 
+@for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "STAGE4_TICKS=%%i"
+@powershell -NoProfile -Command "Write-Host '=== Stage 4/6: Package src ===' -ForegroundColor Blue"
 cd ..\..\..\..\ || exit /b 1
 jar -cMf source.zip source || exit /b 1
 mkdir %RESULTS_DIR%\source || exit /b 1
 move source.zip %RESULTS_DIR%\source\source-%WFE_VERSION%.zip || exit /b 1
 
+@for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "STAGE5_TICKS=%%i"
+@powershell -NoProfile -Command "Write-Host '=== Stage 5/6: Build distr ===' -ForegroundColor Blue"
 cd source\projects\installer\windows\ || exit /b 1
 
-echo Build distr
 call mvn clean package -Dwfe.edition=%WFE_EDITION% -Dwfe.buildhash=%BUILD_HASH% -Djdk.dir="%~dp0jdk" -Dstatistic.report.url=%STATISTIC_REPORT_URL% -Dstatistic.report.days.after.error=%STATISTIC_REPORT_DAYS_AFTER_ERROR% || exit /b 1
 
+@for /f "usebackq" %%i in (`powershell -NoProfile -Command "(Get-Date).Ticks"`) do @set "STAGE6_TICKS=%%i"
+@powershell -NoProfile -Command "Write-Host '=== Stage 6/6: Post-process ===' -ForegroundColor Blue"
 echo Remove .git after build
 cd /D %BUILD_DIR% || exit /b 1
 rd /S /Q source\projects\wfe\.git  || exit /b 1
@@ -126,5 +137,25 @@ jar -cMf runawfe-rtn-linux64-%WFE_VERSION%.zip rtn  || exit /b 1
 rd /S /Q rtn  || exit /b 1
 move runawfe-rtn-linux64-%WFE_VERSION%.zip %RESULTS_DIR%\bin\rtn\runawfe-rtn-linux64-%WFE_VERSION%.zip  || exit /b 1
 
-@echo Autobuild finished at %TIME%
-@powershell -NoProfile -Command "$d = [TimeSpan]::FromTicks((Get-Date).Ticks - %START_TICKS%); Write-Host ('Total build time: {0} hours, {1} minutes, {2} seconds' -f $d.Hours, $d.Minutes, $d.Seconds)"
+@echo ========================================
+@echo            BUILD TIME SUMMARY
+@echo ========================================
+@powershell -NoProfile -Command ^
+  "$s1 = [TimeSpan]::FromTicks(%STAGE2_TICKS% - %STAGE1_TICKS%);" ^
+  "$s2 = [TimeSpan]::FromTicks(%STAGE3_TICKS% - %STAGE2_TICKS%);" ^
+  "$s3 = [TimeSpan]::FromTicks(%STAGE4_TICKS% - %STAGE3_TICKS%);" ^
+  "$s4 = [TimeSpan]::FromTicks(%STAGE5_TICKS% - %STAGE4_TICKS%);" ^
+  "$s5 = [TimeSpan]::FromTicks(%STAGE6_TICKS% - %STAGE5_TICKS%);" ^
+  "$now = (Get-Date).Ticks;" ^
+  "$s6 = [TimeSpan]::FromTicks($now - %STAGE6_TICKS%);" ^
+  "$total = [TimeSpan]::FromTicks($now - %START_TICKS%);" ^
+  "Write-Host '';" ^
+  "Write-Host ('1/6 Prepare:       {0}h {1}m {2}s' -f $s1.Hours, $s1.Minutes, $s1.Seconds) -ForegroundColor Gray;" ^
+  "Write-Host ('2/6 Clone:         {0}h {1}m {2}s' -f $s2.Hours, $s2.Minutes, $s2.Seconds) -ForegroundColor Gray;" ^
+  "Write-Host ('3/6 Update version:{0}h {1}m {2}s' -f $s3.Hours, $s3.Minutes, $s3.Seconds) -ForegroundColor Gray;" ^
+  "Write-Host ('4/6 Package src:   {0}h {1}m {2}s' -f $s4.Hours, $s4.Minutes, $s4.Seconds) -ForegroundColor Gray;" ^
+  "Write-Host ('5/6 Build distr:   {0}h {1}m {2}s' -f $s5.Hours, $s5.Minutes, $s5.Seconds) -ForegroundColor Gray;" ^
+  "Write-Host ('6/6 Post-process:  {0}h {1}m {2}s' -f $s6.Hours, $s6.Minutes, $s6.Seconds) -ForegroundColor Gray;" ^
+  "Write-Host '';" ^
+  "Write-Host ('Total build time:  {0}h {1}m {2}s' -f $total.Hours, $total.Minutes, $total.Seconds) -ForegroundColor Green"
+
